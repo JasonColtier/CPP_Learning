c0c962e356dfde7a60e6797436c75a5cc02c8d3a
Jason <jasoncoltier@gmail.com>
2021-11-14 20:18:13 +0100

    1st question cin

--- Exercices/Questions/QuestionSimples.cpp
+++ Exercices/Questions/QuestionSimples.cpp
@@ -1,8 +1,21 @@
+#include <iostream>
+
+
+
 int main()
 {
 
-    //Write code for summing up indetermined number of ints from standard input.
+    //1. Write code for summing up indetermined number of ints from standard input.$
+    /*
+    int sum=0;
+    int num;
     
+    std::cout<<"Enter numbers to add : "<<std::endl;
+    while(std::cin >> num){
+        sum += num;
+    }
+    std::cout<<"sum : "<<sum<<std::endl;
+    */
 
 
     return 0;
